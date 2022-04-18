177998d197bb4533b8d9eb1aaa92cf75d72654b0
Joosep Arrak <jooseparrak@gmail.com>
2022-04-18 17:17:12 +0300

    Footer changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,12 +3,12 @@ import React from 'react'
 function Footer() {
   return (
     <footer className="flex w-full flex-col items-center bg-[#F7F7F7] p-5 md:items-end">
-      <p className="font-light">&copy; Keidy Arrak 2022</p>
+      <p className="font-light text-slate-700">&copy; Keidy Arrak 2022</p>
       <a
         href="https://www.arrak.dev/?from=kabeauty"
         target="_blank"
         rel="author"
-        className="mt-2 text-sm font-light text-slate-500"
+        className="mt-2 text-xs font-light text-slate-500"
       >
         Disain &#38; ehitus: Joosep Arrak
       </a>
